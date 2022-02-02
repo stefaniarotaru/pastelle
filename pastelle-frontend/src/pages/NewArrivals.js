2beae642fa16c76776e04d2689dd205a7077af36
stefaniarotaru <rotaru.g.stefania@gmail.com>
2022-02-02 14:39:55 +0200

    Style placeholder page for 'New Arrivals'

---
+++ pastelle-frontend/src/pages/NewArrivals.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+
+const NewArrivals = () => {
+    return (
+        <>
+        <Navbar/>
+            <div className="absolute 
+            -top-40 
+            bottom-0 
+            left-0 
+            right-0 
+            m-auto 
+            h-20
+            text-pink-400
+            text-center
+            text-6xl">
+                Under Construction
+            </div>
+            <Footer/>
+        </>
+    )
+}
+
+export default NewArrivals
