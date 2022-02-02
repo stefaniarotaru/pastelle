4400f660d303a07717b325d26101e57b2521eb60
stefaniarotaru <rotaru.g.stefania@gmail.com>
2022-02-02 14:43:37 +0200

    Change hamburger menu icon color

--- pastelle-frontend/src/components/Navbar.js
+++ pastelle-frontend/src/components/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = ({ toggle }) => {
         <svg
           className="w-8 h-8"
           fill="none"
-          stroke="pink"
+          stroke="#f9a8d4"
           viewBox="0 0 24 24"
           xmlns="http://www.w3.org/2000/svg"
         >
