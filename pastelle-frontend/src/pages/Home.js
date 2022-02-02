5af7f09c2f56e77bbf9b6bb6f7329de671681722
stefaniarotaru <rotaru.g.stefania@gmail.com>
2022-02-02 14:41:19 +0200

    Add search bar and filter (FE)

--- pastelle-frontend/src/pages/Home.js
+++ pastelle-frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 // import logo from './logo.svg';
 import '../App.css';
 import Navbar from '../components/Navbar';
@@ -6,6 +6,8 @@ import Dropdown from '../components/Dropdown';
 import Slider from '../components/Slider';
 import Footer from '../components/Footer';
 import Items from '../components/Items';
+import ColorsCheck from '../components/ColorsCheck';
+
 
 
 
@@ -17,7 +19,7 @@ function Home() {
 
   useEffect(() => {
     const hideMenu = () => {
-      if(window.innerWidth > 768 && isOpen) {
+      if (window.innerWidth > 768 && isOpen) {
         setIsOpen(false)
         console.log('i resizedd')
       }
@@ -31,12 +33,34 @@ function Home() {
 
   return (
     <div className="Home">
-      <Navbar toggle={toggle}/>
+      <Navbar toggle={toggle} />
       <Dropdown isOpen={isOpen} toggle={toggle} />
-      <Slider/>
-      <Items/>
+      <Slider />
 
-      <Footer/>
+
+      <div className="my-20 container mx-auto p-5">
+        <div className='mb-8 flex flex-row gap-x-8'>
+
+          <div className='flex flex-row items-center mx-2 gap-x-1'>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#db2777">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+            <input type='text' className='rounded-lg' placeholder='Search' />
+          </div>
+
+          <select className='rounded-lg'>
+            <option>Filter</option>
+            <option>Price ascending</option>
+            <option>Price descending</option>
+            <option>Newest</option>
+          </select>
+
+        </div>
+        <Items />
+      </div>
+
+      <Footer />
+
     </div>
   );
 }
