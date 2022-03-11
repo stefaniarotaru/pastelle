f5dacb03838d697b40fdb223f2a2693ccee28b81
stefaniarotaru <rotaru.g.stefania@gmail.com>
2022-03-11 13:12:51 +0200

    Media icons for login modal

---
+++ pastelle-frontend/src/components/MediaIcons.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MediaIcons = () => {
+    return (
+        <div className='mb-8 mt-4 flex gap-x-6 justify-center'>
+        <img src="https://img.icons8.com/fluency/48/4a90e2/facebook-new.png" />
+        <img src="https://img.icons8.com/color/48/4a90e2/google-logo.png" />
+        <img src="https://img.icons8.com/fluency/48/4a90e2/instagram-new.png" />
+    </div>
+    )
+}
+
+export default MediaIcons
