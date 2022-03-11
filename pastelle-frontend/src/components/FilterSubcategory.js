5dd6aa14bfb9471deb4c8b93f82f0e5ab5fab982
stefaniarotaru <rotaru.g.stefania@gmail.com>
2022-03-11 13:10:45 +0200

    Refactor subcategory filter

--- pastelle-frontend/src/components/FilterSubcategory.js
+++ pastelle-frontend/src/components/FilterSubcategory.js
@@ -5,7 +5,7 @@ const FilterSubcategory = ({ filterCards, types }) => {
     return (
         <div className="flex justify-between mx-6 md:flex-col gap-y-10">
             
-            {types.map((type, i) => <p key={i} onClick={() => filterCards('subcategory', type.db_name)}
+            {types.map((type, i) => <p key={i} onClick={() => {filterCards('subcategory', type.db_name)}}
                 className="hover:text-pink-400 active:text-pink-400 w-36 cursor-pointer">{type.displayName}</p>)}
 
         </div>
