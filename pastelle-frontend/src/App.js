1a5b48c97f9179f11bd5eca4e917ff75b45c3d2c
stefaniarotaru <rotaru.g.stefania@gmail.com>
2022-03-11 13:02:41 +0200

    FE - Refactor category pages into a single one

--- pastelle-frontend/src/App.js
+++ pastelle-frontend/src/App.js
@@ -2,26 +2,56 @@ import './App.css';
 import Home from './pages/Home';
 import Product from './pages/Product';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Apparel from './pages/Apparel';
-import Accessories from './pages/Accessories';
-import Sale from './pages/Sale';
 import NewArrivals from './pages/NewArrivals';
 import Admin from './pages/Admin';
 import Subcategory from './components/Subcategory';
+import { useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
+import Dropdown from './components/Dropdown';
+import Footer from './components/Footer';
+import UserPage from './pages/UserPage';
+import ProductsWithFilters from './pages/ProductsWithFilters';
+import types from './components/Types';
 
 function App() {
 
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  }
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false)
+        console.log('i resizedd')
+      }
+    }
+    window.addEventListener('resize', hideMenu);
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    }
+  })
+
   return (
     <div className='flex flex-col min-h-screen'>
+
       <Router>
+        <Navbar toggle={toggle} />
+        <Dropdown isOpen={isOpen} toggle={toggle} />
+
         <Route exact path="/"><Home /></Route>
         <Route path="/product/:id"><Product /></Route>
-        <Route path="/apparel"><Apparel /></Route>
-        <Route path="/accessories"><Accessories /></Route>
-        <Route path="/sale"><Sale /></Route>
+        <Route path="/accessories"><ProductsWithFilters defaultFilter={'category:accessories'} filterTypes={types.slice(5)} title={'Accessories'}/></Route>
+        <Route path="/apparel"><ProductsWithFilters defaultFilter={'category:apparel'} filterTypes={types.slice(0, 5)} title={'Apparel'}/></Route>
+        <Route path="/sale"><ProductsWithFilters defaultFilter={'sale:true'} filterTypes={types} title={'Sale'}/></Route>
         <Route path="/new"><NewArrivals /></Route>
         <Route path="/admin"><Admin /></Route>
         <Route path="/subcategory/:subcategory"><Subcategory /></Route>
+        <Route path="/user"><UserPage /></Route>
+
+
+        <Footer/>
       </Router>
 
     </div>
---
+++ pastelle-frontend/src/pages/ProductsWithFilters.js
@@ -0,0 +1,133 @@
+import { useEffect, useState } from 'react';
+import React from 'react'
+import axios from "axios";
+import Card from "../components/Card";
+import FilterColor from "../components/FilterColor";
+import FilterSubcategory from "../components/FilterSubcategory";
+import colors from "../components/Colors";
+import FilterTags from '../components/FilterTags';
+import FilterPrice from '../components/FilterPrice';
+import { useAtom } from 'jotai';
+import { PRICES } from '../Store';
+import FilterPrice2 from '../components/FilterPrice2';
+
+
+const ProductsWithFilters = ({ defaultFilter, filterTypes, title }) => {
+
+    const searchBaseUrl = `http://localhost:8080/product/products?search=${defaultFilter},`;
+
+    const [filters, setFilters] = useState({});
+
+    const [cards, setCards] = useState([]);
+
+    const [priceRange, setPriceRange] = useState({});
+
+    // const [atomPrices, setAtomPrices] = useAtom(PRICES);
+
+    const buildSearchUrl = (filters) => {
+        let newSearchUrl = searchBaseUrl;
+        for (const filterKey in filters) {
+            if (filterKey === 'subcategory' || filterKey === 'color') {
+                newSearchUrl = newSearchUrl.concat(filterKey, ":", filters[filterKey], ",")
+            } else if (filterKey === 'price') {
+                newSearchUrl = newSearchUrl.concat(filterKey, "<", filters[filterKey[0]], ",", filterKey, ">", filters[filterKey[1]],)
+            }
+ 
+        }
+        return newSearchUrl
+    }
+
+    useEffect(() => {
+        axios.get(searchBaseUrl).then(res => {
+            setCards(res.data)
+            showPrices(res.data)
+            // const prices = res.data.map(card => card.price)
+            // const minPrice = Math.min(...prices)
+            // const maxPrice = Math.max(...prices)
+
+            // setPriceRange({ min: minPrice, max: maxPrice })
+
+            // console.log("min, max, ", minPrice, maxPrice)
+        })
+    }, [searchBaseUrl])
+
+    const showPrices = (cards) => {
+        const prices = cards.map(card => card.price)
+        const minPrice = Math.min(...prices)
+        const maxPrice = Math.max(...prices)
+        console.log("show prices: ", { min: minPrice, max: maxPrice })
+
+        setPriceRange({ min: minPrice, max: maxPrice })
+    }
+
+    const filterCards = (key, value) => {
+        let newFilters = { ...filters, [key]: value }
+        let newSearchUrl = buildSearchUrl(newFilters)
+
+        setFilters(newFilters)
+        axios.get(newSearchUrl).then(res => {
+            setCards(res.data)
+            // showPrices(res.data)
+            console.log("url: ", newSearchUrl);
+        })
+    }
+    const removeFilter = (key) => {
+        let newFilters = filters;
+        newFilters[key] = '';
+        let newSearchUrl = buildSearchUrl(newFilters)
+        setFilters(newFilters)
+        axios.get(newSearchUrl).then(res => {
+            setCards(res.data)
+
+        })
+    }
+
+
+    return (
+        <>
+            <div className="my-20 container mx-auto p-5 ">
+
+
+
+                <div className="text-2xl my-5">
+                    {title}
+                </div>
+                <div className='md:ml-52 ml-6 flex flex-row items-center md:-mt-8'>
+                    <p className='mr-2 '>Color :</p>
+
+                    <FilterColor
+                        filterCards={filterCards}
+                        colors={colors}
+                    />
+                    <FilterTags filters={filters} removeFilter={removeFilter} />
+                    <div className='ml-8 pt-6'>
+                        {
+                            priceRange?.min ?
+                                <FilterPrice min={priceRange.min} max={priceRange.max} filterCards={filterCards}/>
+                                // <FilterPrice2 min={priceRange.min} max={priceRange.max}/>
+                                : ''
+                        }
+                    </div>
+                </div>
+
+                <div className="md:flex md:flex-row items-start">
+
+                    <div className="mr-4 md:flex md:flex-col gap-y-6 w-80">
+                        <div className="flex mx-6 md:flex-col gap-y-10 gap-x-10 my-6 flex-row flex-wrap w-[40rem] md:w-[8rem]">
+                            <FilterSubcategory filterCards={filterCards} types={filterTypes} />
+
+                        </div>
+                    </div>
+
+                    <div className="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 mt-10">
+                        {cards.length >= 1 ?
+                            cards.map(card => <Card key={card.id} card={card} />) :
+                            <p className="text-3xl w-96 mt-40 text-pink-500">Couldn't find anything! :(</p>}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProductsWithFilters
